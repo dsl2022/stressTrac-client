28b6c5b8d10e3ae7e3a4cb61d249f70a9d8058d5
JizongL <2015rpro@gmail.com>
2019-05-08 18:10:44 -0400

    added registrationform smoke test,all smoke tests are done

---
+++ src/routes/RegistrationPage/RegistrationPage.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RegistrationPage from './RegistrationPage';
+
+
+
+jest.mock('react-plotly.js', () => () => <div/>)
+jest.mock('react-particles-js', () => () => <div/>)
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RegistrationPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
