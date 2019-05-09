a620012806ef1cf7caaaaf094bc9fd192f016e19
JizongL <2015rpro@gmail.com>
2019-05-09 00:49:25 -0400

    updated stress_score in eventlistitem

--- src/components/EventListItem/EventListItem.js
+++ src/components/EventListItem/EventListItem.js
@@ -6,12 +6,16 @@ export default class EventListItem extends Component{
   render(){
     const{event} = this.props
     let eventLevel
-    if(event.stress_score===1)
+    if(event.stress_score===0)
         eventLevel='Mild'
-    if(event.stress_score===2)
-        eventLevel='Severe'
+    if(event.stress_score===1)
+        eventLevel='Moderate'
+    if(event.stress_score===2)        
+        eventLevel='severe'
     if(event.stress_score===3)        
-        eventLevel='acute'
+        eventLevel='acute'    
+    if(event.stress_score===4)        
+        eventLevel='super acute'
     return(
       
       <Link to={`/events/${event.id}`} >
